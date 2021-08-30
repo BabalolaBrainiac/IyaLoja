108a281709d7f150f15f87b0d11821c10a1a3bbb
babalolabrainiac <babalolaopedaniel@gmail.com>
2021-08-30 11:29:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   ENV: process.env.NODE_ENV || "development",
   URL: process.env.BASE_URL || "http://localhost:5000",
-  MONGODB_URI: "mongodb+srv://ajoke:eyimofe@testing.1iehc.mongodb.net/test?retryWrites=true&w=majority",
+  MONGODB_URI:
+    "mongodb+srv://ajoke:eyimofe@testing.1iehc.mongodb.net/test?retryWrites=true&w=majority",
 };
