e0e4c27bdfaece17391dfac763e7084855580ff9
babalolabrainiac <babalolaopedaniel@gmail.com>
2021-09-01 15:19:55 +0100

    Updated User Schema

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema({
     match:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
-  portfolio: { type: [], ref: "Token" },
+  portfolio: { type: [], ref: "Portfolio" },
 });
 
 module.exports = mongoose.model("User", userSchema);
