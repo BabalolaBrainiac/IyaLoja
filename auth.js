787c2e82a96d202865321cacdc2973f8034c11ca
babalolabrainiac <babalolaopedaniel@gmail.com>
2021-09-01 15:04:15 +0100

    Handled errors(Using Restify Error Handler)

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const errors = require("restify-errors");
 const generator = require("generate-password");
 
 //Google Client
@@ -8,6 +10,7 @@ const GOOGLE_CLIENT_ID =
   "508398302179-r7gjngaaqdo9e3jtr6hh75jt0kaaok5u.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "0hAzATNVV5z01DfT0B-0yt_w";
 
+//Google Passport Strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -16,9 +19,41 @@ passport.use(
       callbackURL: "http://localhost:3000/auth/users",
       passReqToCallback: true,
     },
-    (accessToken, refreshToken, profile, done) => {
+    (accessToken, refreshToken, profile, email, done) => {
       console.log("Callback function called");
-      console.log(profile);
+      console.log(email, profile);
+
+      // User Password Strategy(This will generate a new passoword for the new Google user)
+      const newPassword = generator.generate({
+        length: 15,
+        numbers: true,
+      });
+
+      //Hashing New User Password(This will hash the generated password, making it more secure)
+      bcrypt.hash(newPassword, 0, (err, hash) => {
+        if (err) {
+          return next(
+            new errors.InternalServerError("Password Could Not be Saved")
+          );
+        } else {
+          return hash;
+        }
+      });
+      //Create new google User
+      new User({
+        username: profile.displayName,
+        _id: profile.id,
+        password: hash,
+        email: profile.email,
+      })
+        .save()
+        .then((user) => {
+          console.log(user);
+          res.status(200).json("User has been Created");
+        })
+        .catch((err) => {
+          return new errors.InternalServerError("User Could not be Created");
+        });
     }
   )
 );
